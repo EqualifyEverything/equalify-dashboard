5c8658b4dc0620542ef7d60eebc70b74e6ee55da
adonis <lacurly00@gmail.com>
2024-04-15 11:48:58 -0500

    feat(router): initialize routing with React Router and React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import '#src/index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import '~/globals.css';
+import '~/amplify.config';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import NotFound from '~/components/layout/404';
+import Root from '~/routes/root';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [{}],
+  },
+]);
+
+const domNode = document.getElementById('root')!;
+const root = createRoot(domNode);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
--- src/routes/Home.tsx
+++
@@ -1,5 +0,0 @@
-export const Home = () => {
-    return (<div className="pt-24">
-        <img className='w-[300px]' src='/logo.png' />
-    </div>)
-}
