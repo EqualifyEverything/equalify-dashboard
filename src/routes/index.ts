9c3cf5279795eb642f0a7758bccf0747798f46cc
adonis <lacurly00@gmail.com>
2024-04-18 17:41:16 -0500

    Refactor route imports and add Protected component to Root route

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export { default as Root } from './root';
+export { default as Public } from './public/public';
+export { default as Account } from './protected/account';
+export { default as Protected } from './protected/protected';
+
+export { default as Login } from './public/auth/login';
+export { default as Signup } from './public/auth/signup';
