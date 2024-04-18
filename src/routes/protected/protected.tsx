a298c78386831614db9adac4f466b7ce5b995bf3
adonis <lacurly00@gmail.com>
2024-04-18 17:40:24 -0500

    feat(account): Add account page with delete account functionality

---
+++ src/routes/protected/protected.tsx
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom';
+
+import { useAuth } from '~/hooks/useAuth';
+
+const Protected: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
+};
+
+export default Protected;
