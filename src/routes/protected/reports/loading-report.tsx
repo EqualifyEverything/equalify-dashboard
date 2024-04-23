7b80628ca0f4afc6919abc6095cd95a489744d51
adonis <lacurly00@gmail.com>
2024-04-22 21:59:00 -0500

    Add LoadingReport component for displaying loading state in reports

---
+++ src/routes/protected/reports/loading-report.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from '~/components/skeleton/skeleton';
+
+const LoadingReport = () => (
+  <article className="rounded-lg flex flex-col gap-6 bg-white p-4 shadow">
+    <Skeleton className="h-6 w-3/4" />
+    <div className="mt-2 flex items-end justify-between">
+      <div className="space-y-2 w-full">
+        <Skeleton className="h-4 w-3/4" />
+        <Skeleton className="h-4 w-1/2" />
+      </div>
+      <Skeleton className="h-9 w-24" />
+    </div>
+  </article>
+);
+
+export default LoadingReport;
