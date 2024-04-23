055aefe687ff9d2cd6df57fbba27896a67f0749d
adonis <lacurly00@gmail.com>
2024-04-22 22:01:17 -0500

    Add new routes for creating and viewing report details + add place holder routes for report actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 
 import '~/globals.css';
 import '~/amplify.config';
@@ -8,7 +12,7 @@ import '~/amplify.config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { NotFound } from '~/components/layout';
-import { Account, Login, Root, Signup } from '~/routes';
+import { Account, Login, ReportDetails, Reports, Root, Signup } from '~/routes';
 
 const queryClient = new QueryClient();
 
@@ -19,8 +23,36 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       {
+        index: true,
+        element: <Navigate to="/reports" replace />,
+      },
+      {
         path: 'account',
         element: <Account />,
+      },
+      {
+        path: 'reports',
+        element: <Reports />,
+      },
+      {
+        path: 'reports/create-report',
+        element: <div>Create Report</div>,
+      },
+      {
+        path: 'reports/:reportId',
+        element: <ReportDetails />,
+      },
+      {
+        path: 'reports/:reportId/messages/:messageId',
+        element: <div>Message Detail</div>,
+      },
+      {
+        path: 'reports/:reportId/tags/:tagId',
+        element: <div>Tag Detail</div>,
+      },
+      {
+        path: 'reports/:reportId/pages/:pageId',
+        element: <div>Page Detail</div>,
       },
     ],
   },
