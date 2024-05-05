ffa5761602235d08f8cb65905b0b5d394fe55549
adonis <lacurly00@gmail.com>
2024-05-05 04:35:51 -0500

    Update GraphQL types to include Occurrence and Node interfaces

--- src/graphql/types.d.ts
+++ src/graphql/types.d.ts
@@ -1,20 +1,34 @@
 export interface Message {
-    reportId: string;
-    messageId: number;
-    title: string;
-    activeCount: number;
-  }
-  
-  export interface Page {
-    reportId: string;
-    pageId: number;
-    url: string;
-    occurrencesActive: number;
-  }
-  
-  export interface Tag {
-    reportId: string;
-    tagId: number;
-    name: string;
-    referenceCount: number;
-  }
+  reportId: string;
+  messageId: number;
+  title: string;
+  activeCount: number;
+}
+
+export interface Occurrence {
+  messageId: number;
+  title: string;
+  codeSnippet: string;
+  status: string;
+}
+
+export interface Page {
+  reportId: string;
+  pageId: number;
+  url: string;
+  occurrencesActive: number;
+}
+
+export interface Tag {
+  reportId: string;
+  tagId: number;
+  name: string;
+  referenceCount: number;
+}
+
+interface Node {
+  pageUrl: string;
+  pageId: number;
+  codeSnippet: string;
+  status: string;
+}
