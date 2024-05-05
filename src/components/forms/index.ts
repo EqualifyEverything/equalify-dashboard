85dc1d2cd430f9c4795c68a2c1143ad5920ce469
adonis <lacurly00@gmail.com>
2024-05-05 04:34:40 -0500

    Add ReportForm component

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -4,3 +4,4 @@ export { default as AccountForm } from './account-form';
 export { default as LoginForm } from './auth/login-form';
 export { default as SignupForm } from './auth/signup-form';
 export { default as OTPValidationForm } from './auth/otp-validation-form';
+export { default as ReportForm } from './report/report-form';
