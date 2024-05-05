2e0bbc2d9bda3f395f9432d431a688e8e91ee3cb
adonis <lacurly00@gmail.com>
2024-05-05 04:36:50 -0500

    Add new routes for editing and creating reports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,12 @@ export { default as Public } from './public/public';
 export { default as Account } from './protected/account';
 export { default as Protected } from './protected/protected';
 export { default as Reports } from './protected/reports/reports';
+export { default as EditReport } from './protected/reports/edit-report';
+export { default as CreateReport } from './protected/reports/create-report';
 export { default as ReportDetails } from './protected/reports/report-details';
-
+export { default as MessageDetails } from './protected/reports/message-details';
+export { default as TagDetails } from './protected/reports/tag-details';
+export { default as PageDetails } from './protected/reports/page-details';
 
 export { default as Login } from './public/auth/login';
 export { default as Signup } from './public/auth/signup';
