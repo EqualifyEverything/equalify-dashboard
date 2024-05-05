d1f0b9a76175abe1506b4d80071d6fe83903349f
adonis <lacurly00@gmail.com>
2024-05-05 04:37:06 -0500

    Add filterSlice.ts for managing filters in the store

---
+++ src/store/slices/filterSlice.ts
@@ -0,0 +1,28 @@
+import { StateCreator } from 'zustand';
+
+export interface Filter {
+  label: string;
+  value: string;
+}
+
+interface FilterSlice {
+  selectedFilters: Filter[];
+  addFilter: (filter: Filter) => void;
+  removeFilter: (filter: Filter) => void;
+  clearFilters: () => void;
+}
+
+export const createFilterSlice: StateCreator<FilterSlice> = (set) => ({
+  selectedFilters: [],
+  addFilter: (filter) =>
+    set((state) => ({
+      selectedFilters: [...state.selectedFilters, filter],
+    })),
+  removeFilter: (filter) =>
+    set((state) => ({
+      selectedFilters: state.selectedFilters.filter(
+        (f) => f.label !== filter.label || f.value !== filter.value,
+      ),
+    })),
+  clearFilters: () => set({ selectedFilters: [] }),
+});
