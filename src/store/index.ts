d4b59b7d2b81c79ff322e56b8597fe5315c4f877
adonis <lacurly00@gmail.com>
2024-05-05 04:37:17 -0500

    Add filterSlice.ts and createFilterSlice to manage filters in the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,16 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 import { createAuthSlice } from './slices/authSlice';
+import { createFilterSlice } from './slices/filterSlice';
 
-type StoreState = ReturnType<typeof createAuthSlice>;
+type StoreState = ReturnType<typeof createAuthSlice> &
+  ReturnType<typeof createFilterSlice>;
 
 export const useStore = create<StoreState>()(
   persist(
     (...args) => ({
       ...createAuthSlice(...args),
+      ...createFilterSlice(...args),
     }),
     {
       name: 'equalify-store',
