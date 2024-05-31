5d44a3049eeff31679226f646ebf7faa2ecd46f1
adonis <lacurly00@gmail.com>
2024-05-31 00:45:07 -0500

    feat: Add action function for updating and deleting a property

--- src/routes/protected/properties/edit-property.tsx
+++ src/routes/protected/properties/edit-property.tsx
@@ -1,88 +1,175 @@
 import { useState } from 'react';
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
-import { useNavigate, useParams } from 'react-router-dom';
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+  useLoaderData,
+  useNavigate,
+} from 'react-router-dom';
+import { toast } from 'sonner';
 
 import { Button } from '~/components/buttons';
 import { DangerDialog } from '~/components/dialogs';
 import { PropertyForm } from '~/components/forms';
-import { usePropertyDetails } from '~/graphql/hooks/useProperties';
-import * as API from "aws-amplify/api";
+import { propertyQuery } from '~/queries/properties';
+import { deleteProperty, updateProperty } from '~/services/properties';
+import { assertNonNull } from '~/utils/safety';
+import { LoadingProperty } from './loading';
 
-const EditProperty = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const { propertyId } = useParams<{ propertyId: string }>();
-  const { data: propertyDetails, error, isLoading } = usePropertyDetails(propertyId || '');
+/**
+ * Loader function to fetch property data
+ * @param queryClient - The Query Client instance.
+ * @returns Loader function to be used with React Router.
+ */
+export const propertyLoader =
+  (queryClient: QueryClient) =>
+  async ({ params }: LoaderFunctionArgs) => {
+    assertNonNull(
+      params.propertyId,
+      'Property ID is missing in the route parameters',
+    );
 
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-
-  const handleDeleteProperty = async () => {
-    setIsDeleteDialogOpen(false);
-    setLoading(true);
-    console.log(`Deleting property: ${propertyId}`);
-    await API.del({
-      apiName: 'auth', path: '/delete/properties', options: {
-        queryParams: {
-          propertyId
-        }
-      }
-    }).response
-    setLoading(false);
-    navigate('/properties')
+    const initialProperty = await queryClient.ensureQueryData(
+      propertyQuery(params.propertyId),
+    );
+    return { initialProperty, propertyId: params.propertyId };
   };
 
-  const handleSubmit = async (values: { propertyName: string; sitemapUrl: string }) => {
-    console.log('Updating property:', values, propertyId);
-    setLoading(true);
-    const response = await (await API.put({
-      apiName: 'auth', path: '/update/properties', options: {
-        body: {
-          propertyId,
-          propertyName: values.propertyName,
-          sitemapUrl: values.sitemapUrl,
-        }
-      }
-    }).response).body.json();
-    setLoading(false);
-    if (response.status === 'success') {
+/**
+ * Handles updating a property.
+ * @param queryClient - The Query Client instance.
+ * @returns Action function to be used with React Router.
+ */
+export const updatePropertyAction =
+  (queryClient: QueryClient) =>
+  async ({ request, params }: ActionFunctionArgs) => {
+    assertNonNull(params.propertyId, 'No property ID provided');
 
+    try {
+      const formData = await request.formData();
+      const propertyName = formData.get('propertyName') as string;
+      const sitemapUrl = formData.get('sitemapUrl') as string;
+
+      const response = await updateProperty(
+        params.propertyId,
+        propertyName,
+        sitemapUrl,
+      );
+
+      await queryClient.invalidateQueries({
+        queryKey: ['property', params.propertyId],
+      });
+      await queryClient.invalidateQueries({ queryKey: ['properties'] });
+
+      if (response.status === 'success') {
+        toast.success('Property updated successfully!');
+        return redirect(`/properties`);
+      } else {
+        toast.error('Failed to update property.');
+        throw new Response('Failed to update property', { status: 500 });
+      }
+    } catch (error) {
+      toast.error('An error occurred while updating the property.');
+      throw error;
     }
   };
 
-  if (isLoading) return <p>Loading property details...</p>;
-  if (error) return <p>Error loading property details</p>;
+const EditProperty = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { initialProperty, propertyId } = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof propertyLoader>>
+  >;
+
+  const [isFormChanged, setIsFormChanged] = useState(false);
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+
+  const { data: property, isLoading } = useQuery({
+    ...propertyQuery(propertyId!),
+    initialData: initialProperty,
+  });
+
+  const { mutate: deleteMutate } = useMutation({
+    mutationFn: () => deleteProperty(propertyId!),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['properties'] });
+      toast.success('Property deleted successfully!');
+      navigate('/properties');
+    },
+    onError: () => {
+      toast.error('Failed to delete property.');
+    },
+  });
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === 'propertyName' && value.trim() !== property?.name.trim()) {
+      setIsFormChanged(true);
+    } else if (
+      name === 'sitemapUrl' &&
+      value.trim() !== property?.sitemapUrl.trim()
+    ) {
+      setIsFormChanged(true);
+    } else {
+      setIsFormChanged(false);
+    }
+  };
+
+  const handleDeleteProperty = async () => {
+    setIsDeleteDialogOpen(false);
+    deleteMutate();
+  };
 
   return (
     <>
       <h1 id="edit-property-heading" className="text-2xl font-bold md:text-3xl">
-        Edit {propertyDetails?.name || 'Property'}
+        Edit {property?.name || 'Property'}
       </h1>
 
       <section
         aria-labelledby="edit-property-heading"
         className="mt-7 space-y-6 rounded-lg bg-white p-6 shadow"
+        aria-live="polite"
       >
-        <PropertyForm
-          onSubmit={handleSubmit}
-          defaultValues={{ propertyName: propertyDetails?.name || '', sitemapUrl: propertyDetails?.sitemapUrl || '' }}
-          formId="edit-property-form"
-        />
+        {isLoading ? (
+          <LoadingProperty />
+        ) : (
+          <PropertyForm
+            actionUrl={`/properties/${propertyId}/edit`}
+            defaultValues={{
+              propertyName: property?.name || '',
+              sitemapUrl: property?.sitemapUrl || '',
+              propertyDiscovery: 'manually_added',
+            }}
+            formId="edit-property-form"
+            onChange={handleFormChange}
+          />
+        )}
 
         <div className="space-x-6">
+          <Button
+            variant={'outline'}
+            className="w-fit"
+            onClick={() => navigate('/properties')}
+          >
+            Cancel
+          </Button>
           <Button
             type="submit"
             form="edit-property-form"
             className="w-fit bg-[#1D781D] text-white"
-            disabled={loading}
+            disabled={!isFormChanged}
+            aria-disabled={!isFormChanged}
+            aria-live="polite"
           >
-            {!loading ? 'Update Property' : 'Updating...'}
-          </Button>
-          <Button
-            variant={'outline'}
-            className="w-fit"
-            onClick={() => navigate(-1)}
-          >
-            Cancel
+            Update Property
           </Button>
         </div>
       </section>
@@ -90,6 +177,7 @@ const EditProperty = () => {
       <section
         aria-labelledby="danger-zone-heading"
         className="mt-7 space-y-6 rounded-lg bg-white p-6 shadow"
+        aria-live="polite"
       >
         <h2 id="danger-zone-heading" className="text-lg text-[#cf000f]">
           Danger Zone
