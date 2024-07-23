931f30cb3a6f6837bb5f30f838043c60f9ce8519
adonis <lacurly00@gmail.com>
2024-07-22 19:03:35 -0500

    feat: moved toaster component to top of dom

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -3,13 +3,13 @@ import { Footer, Header, MaxWidthWrapper, Announcer } from '.';
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <>
+    <Toaster richColors />
     <Announcer />
     <Header />
     <main className="flex min-h-[calc(100svh-(130px))] flex-col bg-[#e9ecef]">
       <MaxWidthWrapper className="flex-1 py-12">{children}</MaxWidthWrapper>
     </main>
     <Footer />
-    <Toaster richColors />
   </>
 );
 
