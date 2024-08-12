dc891d0b824401805160ea5a08bbda4be023bc67
adonis <lacurly00@gmail.com>
2024-08-11 19:50:33 -0500

    refactor: Update toast imports and components in auth and root layouts

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,15 +1,16 @@
-import { Toaster } from '~/components/alerts/toast';
-import { Footer, Header, MaxWidthWrapper, Announcer } from '.';
+import { Toasts } from '~/components/alerts/toast';
+import { Announcer, Footer, Header, MaxWidthWrapper } from '.';
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <>
-    <Toaster richColors />
-    <Announcer />
-    <Header />
-    <main className="flex min-h-[calc(100svh-(130px))] flex-col bg-[#e9ecef]">
-      <MaxWidthWrapper className="flex-1 py-12">{children}</MaxWidthWrapper>
-    </main>
-    <Footer />
+    <Toasts>
+      <Announcer />
+      <Header />
+      <main className="flex min-h-[calc(100svh-(130px))] flex-col bg-[#e9ecef]">
+        <MaxWidthWrapper className="flex-1 py-12">{children}</MaxWidthWrapper>
+      </main>
+      <Footer />
+    </Toasts>
   </>
 );
 
