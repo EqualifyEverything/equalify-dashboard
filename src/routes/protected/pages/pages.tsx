37db39cda430abf3f9b97db41f0d4ca4357ff8bf
azdak <negatia@gmail.com>
2024-11-18 10:42:11 -0600

    More style cleanup on Pages screen

--- src/routes/protected/pages/pages.tsx
+++ src/routes/protected/pages/pages.tsx
@@ -1,13 +1,11 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
+import {
+  CheckCircledIcon,
+  FileTextIcon,
+  ReloadIcon,
+} from '@radix-ui/react-icons';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { keepPreviousData, QueryClient, useQuery } from '@tanstack/react-query';
-import { Link } from 'react-router-dom';
-
-import { SEO } from '~/components/layout';
-import { pagesQuery } from '~/queries';
-//import { LoadingPages } from './loading';
-import { getPages, getScan, IPage } from '~/services';
-//import DataTable from '~/components/tables/data-table';
-
 import {
   ColumnDef,
   flexRender,
@@ -15,80 +13,160 @@ import {
   PaginationState,
   useReactTable,
 } from '@tanstack/react-table';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '~/components/tables/table';
+//import DataTable from '~/components/tables/data-table';
+
+import {
+  format,
+  formatISO,
+  formatRelative,
+  parse,
+  parseISO,
+  toDate,
+} from 'date-fns';
+import { Link } from 'react-router-dom';
+
+import { SEO } from '~/components/layout';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '~/components/tables/table';
+import { pagesQuery } from '~/queries';
+//import { LoadingPages } from './loading';
+import { getPages, getScan, IPage } from '~/services';
 
 export const pagesLoader = (queryClient: QueryClient) => async () => {
-  const initialPages =
-    await queryClient.ensureQueryData(pagesQuery({ limit: 10, offset: 0 }));
+  const initialPages = await queryClient.ensureQueryData(
+    pagesQuery({ limit: 10, offset: 0 }),
+  );
   return { initialPages };
 };
 
 const Pages = () => {
-
   //const rerender = React.useReducer(() => ({}), {})[1]
 
   // Define the columns
-  const columns = React.useMemo<ColumnDef<IPage>[]> 
-  (
-    () =>
-  [
-    {
-      accessorKey: 'url',
-      header: 'URL',
-      cell: ({ row }) => <a className='text-blue-500 hover:opacity-50' target='_blank' href={row.original.url}>{row.original.url}</a>,
-    },
-    {
-      accessorKey: 'property',
-      header: 'Property',
-      cell: ({ row }) => <span><Link to={`/properties/`+row.original.property?.id+`/edit`}> {row.original.property?.name}</Link></span>,
-    },
-    {
-      accessorKey: 'lastScanned',
-      header: 'Last Scanned At',
-      cell: ({ row }) => <span>{new Date(row.original.scans[0].updated_at).toLocaleString()}</span>,
-    },
-    {
-      accessorKey: 'status',
-      header: 'Status',
-      cell: ({ row }) => <span className={`${row.original.scans[0].processing ? 'bg-[#663808]' : 'bg-[#005031]'} text-white px-2 py-1 rounded-full`}>{row.original.scans[0].processing ? 'Processing' : 'Complete'}</span>,
-    },
-    {
-      accessorKey: 'report',
-      header: 'Raw Data',
-      cell: ({ row }) => row.original.scans[0].processing ? <span className='select-none text-[#666]'>Not ready</span> : <button className='text-blue-500 hover:opacity-50' onClick={async () => {
-        const element = document.getElementById('downloadReportLink');
-        if (element) {
-          const response = await getScan(row.original.scans[0].id);
-          element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(response)));
-          element.setAttribute('download', 'results.json');
-          element.click();
-        }else{
-          console.log("Error fetching scan:", row.original.scans[0].id)
-        }
-      }}>Download</button>,
-    },
-  ], []);
-  
+  const columns = React.useMemo<ColumnDef<IPage>[]>(
+    () => [
+      {
+        accessorKey: 'url',
+        header: 'URL',
+        cell: ({ row }) => (
+          <a
+            className="text-blue-500 hover:opacity-50"
+            target="_blank"
+            href={row.original.url}
+          >
+            {row.original.url}
+          </a>
+        ),
+      },
+      {
+        accessorKey: 'property',
+        header: 'Property',
+        cell: ({ row }) => (
+          <span>
+            <Link to={`/properties/` + row.original.property?.id + `/edit`}>
+              {' '}
+              {row.original.property?.name}
+            </Link>
+          </span>
+        ),
+      },
+      /* {
+        accessorKey: 'lastScanned',
+        header: 'Last Scanned At',
+        cell: ({ row }) => <span>{row.original.scans[0].updated_at}</span>,
+      }, */
+      {
+        accessorKey: 'status',
+        header: 'Status',
+        cell: ({ row }) => (
+          <div
+            
+          >
+            {row.original.scans[0].processing ? (
+              <ReloadIcon aria-label="Processing" />
+            ) : (
+              <Tooltip.Provider>
+                <Tooltip.Root>
+                  <Tooltip.Trigger>
+                    <CheckCircledIcon aria-label="Complete" />
+                  </Tooltip.Trigger>
+                  <Tooltip.Portal>
+                    <Tooltip.Content className="TooltipContent" sideOffset={5}>
+                      Last scanned{' '}
+                      {formatRelative(
+                        parseISO(row.original.scans[0].updated_at),
+                        new Date(),
+                      )}
+                      <Tooltip.Arrow className="TooltipArrow" />
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                </Tooltip.Root>
+              </Tooltip.Provider>
+            )}
+          </div>
+        ),
+      },
+      {
+        accessorKey: 'report',
+        header: 'Raw Data',
+        cell: ({ row }) =>
+          row.original.scans[0].processing ? (
+            <span className="select-none text-[#666]">Not ready</span>
+          ) : (
+            <button
+              className="text-blue-500 hover:opacity-50 inline-flex items-center"
+              onClick={async () => {
+                const element = document.getElementById('downloadReportLink');
+                if (element) {
+                  const response = await getScan(row.original.scans[0].id);
+                  element.setAttribute(
+                    'href',
+                    'data:text/json;charset=utf-8,' +
+                      encodeURIComponent(JSON.stringify(response)),
+                  );
+                  element.setAttribute('download', 'results.json');
+                  element.click();
+                } else {
+                  console.log('Error fetching scan:', row.original.scans[0].id);
+                }
+              }}
+            >
+              Download <FileTextIcon className="ml-1" aria-label="Download" />
+            </button>
+          ),
+      },
+    ],
+    [],
+  );
+
   // pagination
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 10,
-  })
- 
+  });
+
   // data fetching
   const dataQuery = useQuery({
     queryKey: ['pages', pagination],
     queryFn: async () => {
-      const theParams = { limit: pagination.pageSize, offset: pagination.pageIndex*pagination.pageSize };
+      const theParams = {
+        limit: pagination.pageSize,
+        offset: pagination.pageIndex * pagination.pageSize,
+      };
       console.log(theParams);
-      return getPages({params: theParams })
+      return getPages({ params: theParams });
     },
-    placeholderData: keepPreviousData
-  })
+    placeholderData: keepPreviousData,
+  });
 
-  const defaultData = React.useMemo(() => [], [])
+  const defaultData = React.useMemo(() => [], []);
 
-  
   const table = useReactTable({
     data: dataQuery.data?.pages ?? defaultData,
     columns,
@@ -101,8 +179,7 @@ const Pages = () => {
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true, //we're doing manual "server-side" pagination
     debugTable: true,
-  })
-
+  });
 
   return (
     <>
@@ -118,7 +195,7 @@ const Pages = () => {
         >
           Pages
         </h1>
-        <div className='flex flex-row items-center gap-2'>
+        <div className="flex flex-row items-center gap-2">
           <Link
             to="/pages/add"
             className="inline-flex h-9 items-center justify-end place-self-end whitespace-nowrap rounded-md bg-[#005031] px-4 py-3 text-base text-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#1D781D] focus-visible:ring-offset-2 max-sm:w-fit max-sm:px-3 max-sm:py-2.5"
@@ -127,10 +204,8 @@ const Pages = () => {
           </Link>
         </div>
       </div>
-      
-       
-       
-       {/* <div className="mt-7 text-center">
+
+      {/* <div className="mt-7 text-center">
           <h2 className="text-xl font-semibold text-gray-700">
             No Properties Added
           </h2>
@@ -145,35 +220,33 @@ const Pages = () => {
             Add Your First Property
           </Link>
         </div>  */}
-        <section
-          aria-labelledby="pages-list-heading"
-          className="mt-7 space-y-6 rounded-lg bg-white p-6 shadow"
-        >
-          
-          <div className="w-full overflow-x-auto">
+      <section
+        aria-labelledby="pages-list-heading"
+        className="mt-7 space-y-6 rounded-lg bg-white p-6 shadow"
+      >
+        <div className="w-full overflow-x-auto">
           <div className="p-2">
-
-        <Table role="table" aria-label="Pages List">
-          <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id} role="columnheader">
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              ))}
-          </TableHeader>
-          <TableBody>
-              {table.getRowModel().rows?.length ? (
+            <Table role="table" aria-label="Pages List">
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id} role="columnheader">
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext(),
+                              )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
                   table.getRowModel().rows.map((row) => (
                     <TableRow
                       key={row.id}
@@ -201,86 +274,89 @@ const Pages = () => {
                     </TableCell>
                   </TableRow>
                 )}
-          </TableBody>
-        </Table>
-        <nav role="navigation" aria-label="Pagination Navigation" className="flex items-center gap-2">
-          <button
-            className="border rounded p-1"
-            onClick={() => table.firstPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            {'<<'}
-          </button>
-          <button
-            className="border rounded p-1"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            {'<'}
-          </button>
-          <button
-            className="border rounded p-1"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            {'>'}
-          </button>
-          <button
-            className="border rounded p-1"
-            onClick={() => table.lastPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            {'>>'}
-          </button>
-          <span className="flex items-center gap-1">
-            <div>Page</div>
-            <strong>
-              {table.getState().pagination.pageIndex + 1} of{' '}
-              {table.getPageCount().toLocaleString()}
-            </strong>
-          </span>
-          <span className="flex items-center gap-1">
-            | Go to page:
-            <input
-              type="number"
-              min="1"
-              max={table.getPageCount()}
-              defaultValue={table.getState().pagination.pageIndex + 1}
-              onChange={e => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0
-                table.setPageIndex(page)
-              }}
-              className="border p-1 rounded w-16"
-            />
-          </span>
-          <select
-            value={table.getState().pagination.pageSize}
-            onChange={e => {
-              table.setPageSize(Number(e.target.value))
-            }}
-          >
-            {[10, 20, 30, 40, 50].map(pageSize => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
-          {dataQuery.isFetching ? 'Loading...' : null}
-          </nav>
-      
-      <div>
-        Showing {table.getRowModel().rows.length.toLocaleString()} of{' '}
-        {dataQuery.data?.total.toLocaleString()}
-      </div>
-    
-    </div>
+              </TableBody>
+            </Table>
+            <nav
+              role="navigation"
+              aria-label="Pagination Navigation"
+              className="flex items-center gap-2"
+            >
+              <button
+                className="rounded border p-1"
+                onClick={() => table.firstPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                {'<<'}
+              </button>
+              <button
+                className="rounded border p-1"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                {'<'}
+              </button>
+              <button
+                className="rounded border p-1"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                {'>'}
+              </button>
+              <button
+                className="rounded border p-1"
+                onClick={() => table.lastPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                {'>>'}
+              </button>
+              <span className="flex items-center gap-1">
+                <div>Page</div>
+                <strong>
+                  {table.getState().pagination.pageIndex + 1} of{' '}
+                  {table.getPageCount().toLocaleString()}
+                </strong>
+              </span>
+              <span className="flex items-center gap-1">
+                | Go to page:
+                <input
+                  type="number"
+                  min="1"
+                  max={table.getPageCount()}
+                  defaultValue={table.getState().pagination.pageIndex + 1}
+                  onChange={(e) => {
+                    const page = e.target.value
+                      ? Number(e.target.value) - 1
+                      : 0;
+                    table.setPageIndex(page);
+                  }}
+                  className="w-16 rounded border p-1"
+                />
+              </span>
+              <select
+                value={table.getState().pagination.pageSize}
+                onChange={(e) => {
+                  table.setPageSize(Number(e.target.value));
+                }}
+              >
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+              {dataQuery.isFetching ? 'Loading...' : null}
+            </nav>
+
+            <div>
+              Showing {table.getRowModel().rows.length.toLocaleString()} of{' '}
+              {dataQuery.data?.total.toLocaleString()}
+            </div>
+          </div>
         </div>
-         
-        </section>
-        <a id="downloadReportLink" style={{ display: 'none' }}></a>
+      </section>
+      <a id="downloadReportLink" style={{ display: 'none' }}></a>
     </>
   );
 };
 
 export default Pages;
-
