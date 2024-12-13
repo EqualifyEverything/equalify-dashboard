faaa6b46faabef10bb63a4621b29be9c01329faa
azdak <negatia@gmail.com>
2024-12-13 14:46:18 -0600

    v1 of CSV support for Add Pages

--- src/routes/protected/pages/add-pages.tsx
+++ src/routes/protected/pages/add-pages.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent } from 'react';
 import {
   ArchiveIcon,
   ChevronDownIcon,
@@ -8,19 +8,13 @@ import {
   LaptopIcon,
   PlusCircledIcon,
 } from '@radix-ui/react-icons';
+import * as Label from '@radix-ui/react-label';
 import * as Select from '@radix-ui/react-select';
 import * as Separator from '@radix-ui/react-separator';
 import * as Tabs from '@radix-ui/react-tabs';
 import { QueryClient } from '@tanstack/react-query';
 import { Controller, useFieldArray, useForm } from 'react-hook-form';
-import {
-  ActionFunctionArgs,
-  Link,
-  redirect,
-  useLoaderData,
-  useNavigate,
-} from 'react-router-dom';
-import * as Label from "@radix-ui/react-label";
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 
 import { toast } from '~/components/alerts';
 import { SEO } from '~/components/layout';
@@ -33,33 +27,44 @@ export const addPagesLoader = (queryClient: QueryClient) => async () => {
   return { initialProperties };
 };
 
+const URL_REGEX = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+
 const AddPages = () => {
   const navigate = useNavigate();
   const { initialProperties } = useLoaderData() as Awaited<
     ReturnType<ReturnType<typeof addPagesLoader>>
   >;
 
-  const { register, control, handleSubmit, reset, watch, setValue, formState: {errors} } = useForm({
-    mode: "onChange",
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
     defaultValues: {
-      urls: [{ url: "" }],
-      sitemapUrl: "",
-      property: "",
-      mode: "url",
+      urls: [{ url: '' }],
+      sitemapUrl: '',
+      property: '',
+      mode: 'url',
     },
   });
 
   const { fields, append, remove } = useFieldArray({
     control,
-    name: "urls",
+    name: 'urls',
   });
   const activeTab = watch('mode');
+  const currentUrls = watch('urls');
   const onSubmit = (data: any) => addPages(data);
 
   const addPages = async (data: any) => {
-    console.log("Sending...", data);
-    const response = await addPagesFromForm({data});
-    console.log("Response...", response);
+    console.log('Sending...', data);
+    const response = await addPagesFromForm({ data });
+    console.log('Response...', response);
     if (response.result.status === 'success') {
       toast.success({
         title: 'Success',
@@ -74,6 +79,74 @@ const AddPages = () => {
     reset();
   };
 
+  const onFileReaderChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    let file;
+    if (target.files && target.files.length) {
+      file = target.files[0];
+    } else {
+      return;
+    }
+    if (file.type !== 'text/csv') {
+      return throwFileError(event, `You must select a CSV file`);
+    }
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      if (e.target) {
+        const text = e.target.result;
+        if (text && typeof text == 'string') {
+          const rows = text.split('\n');
+          const parsedData = rows.map(
+            (cell) => cell.trim(),
+          );
+          const headers = parsedData;
+          if (
+            headers?.[0] !== 'url' 
+          ) {
+            return throwFileError(
+              event,
+              `You must have the first row headers set to "url"`,
+            );
+          }
+          const parsedRows = parsedData.slice(1, parsedData.length).filter(function (el) {
+            return el != null && el != ""; // remove any empty rows
+          });
+          console.log(parsedRows);
+          for (const row of parsedRows) {
+            try {
+              const url = new URL(row);
+              if(!row.match(URL_REGEX)){ throw "URL Matching Error" };
+            } catch (err) {
+              return throwFileError(
+                event,
+                `You have an invalid URL in your CSV`,
+              );
+            }
+          }
+          //setData(parsedData);
+          //console.log(parsedRows);
+          const formattedForExport = parsedRows.map((val)=>{
+            return { url: val }
+          });
+          //console.log(formattedForExport);
+          setValue("urls", formattedForExport)
+        }
+      }
+    };
+    reader.readAsText(file);
+    return;
+  };
+
+  const throwFileError = (
+    event: ChangeEvent<HTMLInputElement>,
+    message: string,
+  ) => {
+    alert(message);
+    //setData([]);
+    event.target.value = '';
+    return;
+  };
+
   return (
     <>
       <SEO
@@ -81,7 +154,6 @@ const AddPages = () => {
         description="Add new pages to Equalify to start monitoring and improving accessibility."
         url="https://dashboard.equalify.app/pages/add"
       />
-      
 
       <div className="flex w-full flex-col-reverse justify-between sm:flex-row sm:items-center">
         <h1
@@ -94,12 +166,13 @@ const AddPages = () => {
           <Link
             to=""
             onClick={() => navigate(-1)}
-            aria-label='Back to Pages'
-            className="text-[#186121]">
+            aria-label="Back to Pages"
+            className="text-[#186121]"
+          >
             &#60; Back to Pages
           </Link>
-        </div> 
-      </div> 
+        </div>
+      </div>
       <section
         aria-labelledby="add-pages-heading"
         className="mt-7 space-y-6 rounded-lg bg-white p-6 shadow"
@@ -147,14 +220,18 @@ const AddPages = () => {
                   return (
                     <li key={item.id} className="flex py-1">
                       <input
-                        aria-invalid={errors && errors.urls && errors.urls[index] ? "true" : "false"}
+                        aria-invalid={
+                          errors && errors.urls && errors.urls[index]
+                            ? 'true'
+                            : 'false'
+                        }
                         className="flex w-full rounded-md border border-gray-200 bg-white px-3 py-1 text-base shadow-sm transition-colors aria-[invalid=true]:border-red-400"
                         {...register(`urls.${index}.url`, {
-                          
-                          required: activeTab == "url",
+                          required: activeTab == 'url',
                           pattern: {
-                            value: /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/,
-                            message: "Invalid URL format"
+                            value:
+                            URL_REGEX,
+                            message: 'Invalid URL format',
                           },
                           /* validate: (value) => {
                             return true;
@@ -163,11 +240,6 @@ const AddPages = () => {
                         })}
                       />
 
-                      {/* <Controller
-                          render={({ field }) => <input {...field} />}
-                          name={`urls.${index}.url`}
-                          control={control}
-                        /> */}
                       <button type="button" onClick={() => remove(index)}>
                         <CrossCircledIcon className="ml-2 opacity-50" />
                       </button>
@@ -180,9 +252,9 @@ const AddPages = () => {
                 onClick={() => {
                   append({ url: '' });
                 }}
-                className='inline-flex justify-center text-center items-center w-full mr-8 bg-slate-100 py-2 rounded-md border border-gray-200 hover:bg-green-200'
+                className="mr-8 inline-flex w-full items-center justify-center rounded-md border border-gray-200 bg-slate-100 py-2 text-center hover:bg-green-200"
               >
-                <PlusCircledIcon className='mr-2'/> Add URL
+                <PlusCircledIcon className="mr-2" /> Add URL
               </button>
             </Tabs.Content>
             <Tabs.Content className="TabsContent p-2" value="sitemap">
@@ -190,13 +262,14 @@ const AddPages = () => {
               Sitemap Input tab Content 
               **********/}
               <input
-                aria-invalid={ errors.sitemapUrl ? "true" : "false"}
+                aria-invalid={errors.sitemapUrl ? 'true' : 'false'}
                 className="flex w-full rounded-md border border-gray-200 bg-white px-3 py-1 text-base shadow-sm transition-colors aria-[invalid=true]:border-red-400"
                 {...register(`sitemapUrl`, {
-                  required: activeTab == "sitemap",
+                  required: activeTab == 'sitemap',
                   pattern: {
-                    value: /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/,
-                    message: "Invalid URL format"
+                    value:
+                      /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/,
+                    message: 'Invalid URL format',
                   },
                   /* validate: (value) => {
                     return true;
@@ -208,13 +281,30 @@ const AddPages = () => {
               {/******* 
               CSV Input tab Content 
               **********/}
-              <p className="Text">CSV</p>
+              Upload a CSV file of pages to add to Equalify. Please review the <a target="_blank" className="underline" href="/template-pages.csv">example CSV template</a> for format.
+              <input onChange={onFileReaderChange} type="file" accept=".csv" />
+              {(currentUrls.length > 0 && currentUrls[0].url !== "") && (
+                <div className="flex flex-col">
+                  <div>CSV successfully uploaded! Showing first 10 rows:</div>
+                  {currentUrls.slice(0, 10).map((row, index) => (
+                    <div
+                      key={index}
+                      className={`flex flex-row gap-2 p-1 ${index === 0 && 'bg-card'}`}
+                    >
+                      <div key={index} className="truncate">
+                        {row.url}
+                      </div>
+                    </div>
+                  ))}
+                  <div>...</div>
+                </div>
+              )}
             </Tabs.Content>
           </Tabs.Root>
           <Separator.Root />
 
           <div className="p-2">
-            <Label.Root htmlFor="property" className='text-xs pr-2'>
+            <Label.Root htmlFor="property" className="pr-2 text-xs">
               Add to Property
             </Label.Root>
             <Controller
@@ -237,14 +327,18 @@ const AddPages = () => {
                         <ChevronUpIcon />
                       </Select.ScrollUpButton>
                       <Select.Viewport className="SelectViewport">
-                        <Select.Item value="none" key="null" className="p-2 cursor-pointer hover:bg-green-100">
+                        <Select.Item
+                          value="none"
+                          key="null"
+                          className="cursor-pointer p-2 hover:bg-green-100"
+                        >
                           <Select.ItemText>None</Select.ItemText>
                         </Select.Item>
                         {initialProperties.map((item, index) => (
                           <Select.Item
                             value={item.id}
                             key={index}
-                            className="p-2 cursor-pointer hover:bg-green-100"
+                            className="cursor-pointer p-2 hover:bg-green-100"
                           >
                             <Select.ItemText>{item.name}</Select.ItemText>
                           </Select.Item>
@@ -259,11 +353,11 @@ const AddPages = () => {
               )}
             />
             <Separator.Root />
-            <div className="flex flex-row items-center gap-2 mt-4">
+            <div className="mt-4 flex flex-row items-center gap-2">
               <input
                 type="submit"
                 value="Add Pages"
-                className="cursor-pointer inline-flex items-center whitespace-nowrap rounded-md bg-[#005031] px-3 py-1 text-base text-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#1D781D] focus-visible:ring-offset-2 max-sm:w-fit max-sm:px-3 max-sm:py-2.5"
+                className="inline-flex cursor-pointer items-center whitespace-nowrap rounded-md bg-[#005031] px-3 py-1 text-base text-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#1D781D] focus-visible:ring-offset-2 max-sm:w-fit max-sm:px-3 max-sm:py-2.5"
               />
               <button
                 type="button"
